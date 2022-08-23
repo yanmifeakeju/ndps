7af6eab54d7bebea43ad2ac037fa5f3cd9e9b047
yanmifeakeju <yanmifeakeju@gmail.com>
2022-08-23 08:41:49 +0100

    excercise to mimic the behavior of promise.ll()

---
+++ excercises/chapter-5/promise-all.mjs
@@ -0,0 +1,39 @@
+/**
+ * 5.1 Dissecting Promise.all(): Implement your own version of Promise.all()
+ * leveraging promises, async/await, or a combination of the two.
+ * The function must be functionally equivalent to its original counterpart.
+ */
+
+import { promisify } from 'util';
+
+const delay = promisify(setTimeout);
+
+const PromiseAll = async (...operations) => {
+  console.log(`Start seconds: ${new Date().getSeconds()}`);
+  const results = [];
+
+  for (let ops of operations) {
+    results.push(await ops);
+  }
+
+  console.log(results);
+  console.log(`Start seconds: ${new Date().getSeconds()}`);
+};
+
+async function promisedFuncs(seconds) {
+  console.log(seconds);
+  await delay(seconds * 1000);
+  return seconds;
+}
+
+async function rejectedPromise() {
+  return Promise.reject(new Error('Rejected.'));
+}
+
+PromiseAll(
+  promisedFuncs(4),
+  promisedFuncs(2),
+  promisedFuncs(1),
+  rejectedPromise(),
+  promisedFuncs(3)
+).then(console.log, console.error);
