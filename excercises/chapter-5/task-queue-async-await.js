f361d96d74b3dab2b0bdcf2a151d90ef00e58f35
yanmifeakeju <yanmifeakeju@gmail.com>
2022-08-24 09:41:08 +0100

    the task runner should handle the error

--- excercises/chapter-5/task-queue-async-await.js
+++ excercises/chapter-5/task-queue-async-await.js
@@ -13,8 +13,12 @@ export class TaskQueue {
   runTask(task) {
     return new Promise((resolve) => {
       this.queue.push(async () => {
-        const t = await task();
-        resolve(t);
+        try {
+          const t = await task();
+          resolve(t);
+        } catch (error) {
+          console.log(error);
+        }
       });
       process.nextTick(this.next.bind(this));
     });
@@ -24,15 +28,8 @@ export class TaskQueue {
     while (this.running < this.concurrency && this.queue.length) {
       const task = this.queue.shift();
       this.running++;
-
-      try {
-        await task();
-      } catch (error) {
-        console.log(error);
-      }
-
+      await task();
       this.running--;
-      this.next();
     }
   }
 }
